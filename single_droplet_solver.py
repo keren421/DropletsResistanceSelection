008379b4861dc7c13a89af052c59254c804d75b2
keren421 <kerenbenzvi@gmail.com>
2018-12-11 15:57:25 +0200

    generalized solver + allowed to see multiple figures

--- single_droplet_solver.py
+++ single_droplet_solver.py
@@ -1,34 +1,39 @@
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
+#plt.ion()
 
-#dn0/dt = -sigma(di*ni)
+#dn0/dt = -n0 * sigma(di*ni)
 #dni/dt = lambda(n0)*ni*(1-sigma(nj)/c)-k(no)*ni
 
 
-def bacteria_growth(t, n,d_rate, mics):
+def bacteria_growth(t, n, d_rate, mics):
     nominal_growth_rate = 1
-    nominal_killing_rate = 10
-
-    growth_rate = [nominal_growth_rate*np.heaviside(mic-n[0],1) for mic in mics]
-    killing_rate = [nominal_killing_rate*np.heaviside(n[0] - mic,1) for mic in mics]
+    nominal_killing_rate = 10 #
     C = 50
-    total_bacteria = n[1] + n[2] + n[3]
-    return [-n[0] * (n[1]*d_rate[1] + n[2]*d_rate[2] + n[3]*d_rate[3]),
-            growth_rate[1]*n[1]*(1-total_bacteria/C) - killing_rate[1]*n[1],
-            growth_rate[2]*n[2]*(1-total_bacteria/C) - killing_rate[2]*n[2],
-            growth_rate[3]*n[3]*(1-total_bacteria/C) - killing_rate[3]*n[3]]
+    total_bacteria = sum(n[1:])
+
+    derivatives = []
+    for j in range(len(n)):
+        if j == 0:
+            derivatives.append(-n[0] * sum([n[bacteria_num]*d_rate[bacteria_num] for bacteria_num in range(1,len(n))]))
+        else:
+            growth_rate = nominal_growth_rate*np.heaviside(mics[j]-n[0], 1)
+            killing_rate = nominal_killing_rate*np.heaviside(n[0] - mics[j], 1)
+            derivatives.append(growth_rate*n[j]*(1-total_bacteria/C) - killing_rate*n[j])
+    return derivatives
 
 
 def single_droplet_solver(initial_pop, d_rate, mics):
     t_end = 20
     growth_in_droplet = lambda t,n: bacteria_growth(t, n, d_rate, mics)
     sol = solve_ivp(growth_in_droplet, (0, t_end), initial_pop)
+    plot_solution(sol)
     return sol
 
 
 def plot_solution(sol):
-    plt.figure(1)
+    f = plt.figure()
     plt.plot(sol.t, sol.y[0], label='Antibiotic')
     plt.plot(sol.t, sol.y[1], label='Sensitive')
     plt.plot(sol.t, sol.y[2], label='Resistant')
@@ -36,20 +41,12 @@ def plot_solution(sol):
     plt.legend()
     plt.ylabel('Population Size')
     plt.xlabel('Time')
-    plt.show()
 
-sensitive_bacteria = {'amount': 5,
-                      'mic': 1,
-                      'd_rate': 0}
-resistant_bacteria = {'amount': 1,
-                      'mic': 10,
-                      'd_rate': 10}
-super_resistant_bacteria = {'amount': 1,
-                            'mic': 100,
-                            'd_rate': 100}
 
-initial_pop = [8, 5, 1,0]
-d_rate = [0, 0, 10, 100]
+initial_pop = [8, 5,  1,   0]
+d_rate = [0, 0,  1,  10]
 mics = [0, 1, 10, 100]
-sol = single_droplet_solver(initial_pop, d_rate, mics)
-plot_solution(sol)
+sol = single_droplet_solver([11, 5, 1, 0], d_rate, mics)
+#sol = single_droplet_solver([8, 5, 1, 0], d_rate, mics)
+print(sol.y[:,-1])
+plt.show()
